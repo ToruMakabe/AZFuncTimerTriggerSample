d531d85f3722767cc28a08e109f134e65e405de0
Toru Makabe <tomakabe@microsoft.com>
2016-09-07 17:53:47 +0900

    re-package test

--- test/index.js
+++ test/index.js
@@ -1,15 +1,21 @@
 'use strict';
 
+const azureStorage = require('azure-storage');
+const myContainer = 'billingdata'
 const now = new Date(Date.now());
+const twoDaysAgo = new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate() -2));
+const startDateForFileName = twoDaysAgo.getUTCFullYear() + '-' + (twoDaysAgo.getUTCMonth() +1) + '-' + twoDaysAgo.getUTCDate(); 
 
 module.exports = function (context, myTimer) {
 
-  context.log('Starting... ', now);
+  const blobSvc = azureStorage.azure.createBlobService(process.env.azfuncpoc_STORAGE);
+  blobSvc.createContainerIfNotExists(myContainer, function(err, result, response){
+    if(err) context.log(err);
+  });
 
-  context.log('My contexts are... ', context);
+  blobSvc.appendFromText(myContainer, startDateForFileName + '.json', 'text to be appended', function(err, result, response){
+    if(!err) context.log(err);
+  });
 
-  context.log('My connection 1 is...', process.env.AzureWebJobsStorage);
-  context.log('My connection 2 is...', process.env.azfuncpoc_STORAGE);
-   
   context.done();
 };
