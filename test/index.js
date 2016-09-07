d2451fb3986a12096bdc6670fd68cb038f8236e7
Toru Makabe <tomakabe@microsoft.com>
2016-09-07 17:16:00 +0900

    start test

--- test/index.js
+++ test/index.js
@@ -2,18 +2,14 @@
 
 const now = new Date(Date.now());
 
-module.exports = function (context, myTimer, myOutputBlob) {
+module.exports = function (context, myTimer) {
 
   context.log('Starting... ', now);
 
   context.log('My contexts are... ', context);
 
-  context.log('My connection is...', process.env[azfuncpoc_STORAGE]);
-
-  context.log('Before copying... ', myOutputBlob);
-
-  myOutputBlob = 'test' ;
-  context.log('After copying... ', myOutputBlob);
+  context.log('My connection 1 is...', process.env.AzureWebJobsStorage);
+  context.log('My connection 2 is...', process.env.azfuncpoc_STORAGE);
    
   context.done();
 };
